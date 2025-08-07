2170aae3824a476e08b936248920e26aa65e6dc2
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-08-06 22:01:54 -0700

    More front end work

--- src/globalComps/topBar/topBar.jsx
+++ src/globalComps/topBar/topBar.jsx
@@ -5,6 +5,7 @@ function TopBar() {
   const navigate = useNavigate();
   const location = useLocation();
   const canGoBack = location.key === 'default';
+  // Need to figure out disabling the forward feature when appropriate
 
   return (
     <section className={styles.topBar}>
@@ -19,6 +20,12 @@ function TopBar() {
             <path d="M0 256a256 256 0 1 0 512 0 256 256 0 1 0 -512 0zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"/>
           </svg>
         </button>
+      </div>
+      <div className={styles.searchBar}>
+        <svg className={styles.icon} aria-hidden="true" viewBox="0 0 24 24">
+        <g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g>
+        </svg>
+        <input placeholder="Search" type="search" className={styles.input}></input>
       </div>
     </section>
   );
